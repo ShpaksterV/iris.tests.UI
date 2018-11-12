6083ada93090dc4164e5228e934e721155e7c427
ShpaksterV <vlad37454294>
2018-11-12 18:24:31 +0200

    mappe

--- src/test/java/Constants/DriverFactory.java
+++ src/test/java/Constants/DriverFactory.java
@@ -2,19 +2,21 @@ package Constants;
 
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
 
     private WebDriver driver;
 
-    public WebDriver getDriver()  {
-        String name = System.getProperty("driver");
-        if (name.equals("chrome")) {
-            driver = new ChromeDriver();
-        }
+    public WebDriver getDriver() throws MalformedURLException {
+
+        driver=new RemoteWebDriver(new URL("http://192.168.1.64:4444/wd/hub"), DesiredCapabilities.chrome());
+
 
         this.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         this.driver.manage().window().maximize();
